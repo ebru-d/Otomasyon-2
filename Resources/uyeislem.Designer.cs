a4b2ee9781a8228ac4b27e448b5162158ab0d486
Ebru Doğan <128515265+ebru-d@users.noreply.github.com>
2025-03-01 17:22:24 +0300

    Add files via upload

---
+++ Resources/uyeislem.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace Otomasyon
+{
+    partial class uyeislem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(uyeislem));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelcaptcha = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelmesaj = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Bisque;
+            this.label1.Location = new System.Drawing.Point(52, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Eski Şifre :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Bisque;
+            this.label2.Location = new System.Drawing.Point(52, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yeni Şifre :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Bisque;
+            this.label3.Location = new System.Drawing.Point(52, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Yeni Şifre(Tekrar) :";
+            // 
+            // labelcaptcha
+            // 
+            this.labelcaptcha.AutoSize = true;
+            this.labelcaptcha.BackColor = System.Drawing.Color.Bisque;
+            this.labelcaptcha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelcaptcha.Location = new System.Drawing.Point(52, 230);
+            this.labelcaptcha.Name = "labelcaptcha";
+            this.labelcaptcha.Size = new System.Drawing.Size(20, 25);
+            this.labelcaptcha.TabIndex = 3;
+            this.labelcaptcha.Text = "*";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(228, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(178, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(228, 134);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(178, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(228, 184);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(178, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox4.Location = new System.Drawing.Point(278, 231);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(128, 27);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 31);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Değiştir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(69, 314);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 31);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Çıkış";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelmesaj
+            // 
+            this.labelmesaj.AutoSize = true;
+            this.labelmesaj.BackColor = System.Drawing.Color.Bisque;
+            this.labelmesaj.Location = new System.Drawing.Point(55, 275);
+            this.labelmesaj.Name = "labelmesaj";
+            this.labelmesaj.Size = new System.Drawing.Size(13, 17);
+            this.labelmesaj.TabIndex = 10;
+            this.labelmesaj.Text = "*";
+            // 
+            // uyeislem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Otomasyon.Properties.Resources.arkaplnn;
+            this.ClientSize = new System.Drawing.Size(481, 463);
+            this.Controls.Add(this.labelmesaj);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelcaptcha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "uyeislem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "uyeislem";
+            this.Load += new System.EventHandler(this.uyeislem_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelcaptcha;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelmesaj;
+    }
+}
---
+++ Resources/uyeislem.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Otomasyon
+{
+    public partial class uyeislem : Form
+    {
+        public uyeislem()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public static string SqlCon = "Data Source = EBRUACER\\SQLEXPRESS;Initial Catalog = eser; Integrated Security = True";
+        int sonuc = 0;
+        public void captchaolustur()
+        {
+            Random r = new Random();
+            int ilk = r.Next(0, 50);
+            int ikinci = r.Next(0, 50);
+            labelcaptcha.Text = ilk.ToString() + "+" + ikinci.ToString() + "=";
+            sonuc = ilk + ikinci;
+            textBox4.Clear();
+        }
+
+        private void uyeislem_Load(object sender, EventArgs e)
+        {
+            
+            captchaolustur();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text == sonuc.ToString())
+            {
+                labelmesaj.Text = " ";
+                if (textBox2.Text == textBox3.Text)
+                {
+                    eskisifrekontrol();
+                }
+                else
+                {
+                    labelmesaj.Text = "Girilen Şifreler Aynı Değil!";
+
+                }
+            }
+            else
+            {
+                labelmesaj.Text = "Captchca Hatalı Girildi..";
+            }
+            
+        }
+        public void eskisifrekontrol()
+        {
+            con = new SqlConnection(SqlCon);
+            string sorgu = "select şifre from ziyaretci_login where tc=@no and şifre=@pass";
+            cmd = new SqlCommand(sorgu, con);
+            cmd.Parameters.AddWithValue("@no", uyelogin.kullaniciSession);
+            cmd.Parameters.AddWithValue("@pass", veritabani.MD5Sifrele(textBox1.Text));
+
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                con = new SqlConnection(SqlCon);
+                string sql = "update ziyaretci_login set şifre= '" + veritabani.MD5Sifrele(textBox2.Text) + "'";
+                cmd = new SqlCommand(sql, con);
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = sql;
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Şifre Başarıyla Değiştirildi..");
+                this.Hide();
+            }
+            else
+            {
+                labelmesaj.Text = "Eski Şifreniz Hatalı..";
+
+
+            }
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            
+        }
+    }
+    }
